2c471fd1d2b4a2bb85bf6f2b12b75dbc75b7ad9f
Mike Griffin <mgriffin@github.com>
2016-11-16 21:34:57 +0000

    How did this ever work? Fixes the title check

--- tests/front_matter_test.rb
+++ tests/front_matter_test.rb
@@ -5,7 +5,7 @@ class FrontMatterTest < Blog::Test
   def test_valid_front_matter
     posts.each do |filename|
       yaml = YAML.load_file(filename)
-      assert yaml[title].exist?
+      assert yaml.has_key?("title")
     end
   end
 end
